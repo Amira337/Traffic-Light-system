45c33987c7a1c88e4e5307d934be0d6038c92aa9
Amira337 <138816787+Amira337@users.noreply.github.com>
2023-07-13 00:30:42 +0300

    Add files via upload

---
+++ fabulous_kup1.ino
@@ -0,0 +1,39 @@
+int button=2;
+int red=3;
+int yellow=4;
+int green=5;
+int Reading=0;
+int counter=0;
+void setup()
+{
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop()
+{
+  Reading=digitalRead(button);
+  
+  if (Reading==HIGH)
+  {
+    counter++;
+    if (counter==1)
+    {digitalWrite(red,HIGH);}
+    else if (counter==2)
+    {digitalWrite(yellow,HIGH);}
+    else if (counter==3)
+    {digitalWrite(green,HIGH);}
+    else
+    {
+      digitalWrite(red,LOW);
+     digitalWrite(yellow,LOW);
+     digitalWrite(green,LOW);
+     
+    counter=0;
+    }
+    delay(250);
+  }
+}
+     
